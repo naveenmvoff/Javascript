cfb8b5d7ddf02e38af03e77037271b4ade496ab1
naveenmvoff <naveenmvoff@gmai.com>
2025-02-17 22:09:03 +0530

    #29 completed

---
+++ 10ForLoops.js
@@ -0,0 +1,33 @@
+// // // // For Loop
+
+// // // for( i = 0; i<=50; i++){
+// // //     if (i % 2 == 0){
+// // //         console.log(i);
+// // //     }
+// // // }
+
+// // let store = 0;
+
+// // for (i = 1; i <= 5; i++) {
+// //     store = store + i;
+// // }
+
+// // console.log(store)
+
+//  /*Write a for loop to print the elements of a given list: numbers = [10, 20, 30, 40, 50]. */
+// numbers = [10, 20, 30, 40, 50];
+
+// for (i = 0; i in numbers; i++){
+//     console.log(numbers[i]);
+// }
+
+
+/*Write a for loop to print each character of a string "HELLO".*/
+
+let word = "HEALTH";
+
+for (i = 0; i < word.length;i++ ){
+    console.log(word[i])
+}
+
+
